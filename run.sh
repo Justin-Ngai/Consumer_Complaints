25cc37cf8be0924c76d789a47831de05d2e50555
Justin <52053803+Justin-Ngai@users.noreply.github.com>
2020-03-20 10:31:39 -0400

    Add files via upload
    Updated to call the latest Python version, & a more expressive SRC file name.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python 3.7
 
-python ./src/consumer_complaints.py ./input/consumer_complaints.csv ./output/report.csv
+python3.8 ./src/get_product-year_stats.py ./input/complaints.csv ./output/report.csv
