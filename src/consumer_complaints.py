97fcdd73c17ebbb8c82150a793228286835f97fe
Justin-Ngai <52053803+Justin-Ngai@users.noreply.github.com>
2020-03-19 21:55:16 -0400

    Add files via upload
    Revised the variable names to be more understandable, 
    & revised/added comments for the overall file, each code block, & specific techniques.

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -1,46 +1,51 @@
+'''
+This file takes a CSV of complaints,
+gets some statistics on these complaints,
+then writes these statistics to an output CSV file.
+'''
+
 import sys, csv
 
-#Code block to read CSV file & prepare a dictionary for aggregation functions
+# Code block to read CSV & prepare dictionary for aggregate functions
 
-i = sys.argv[1] #input file
-with open(i) as file: #opens input file
+with open(sys.argv[1]) as file: # Opens input file
     reader = csv.reader(file)
-    reader.next() #skips header
+    reader.next() # Skips header
+    
     d = {}
     for row in reader:
-        index = (row[1], row[0][:4])
-        if index not in d:
-            d[index] = {row[7]: 1}
-        else:
-            if row[7] not in d[index]:
-                d[index][row[7]] = 1
+        key = (row[1], row[0][:4]) # Each key is product-year pair
+        if key not in d:
+            d[key] = {row[7]: 1} # Each value is companies with this complaint
+        else:                    # & how many complaints for each of these companies
+            if row[7] not in d[key]:
+                d[key][row[7]] = 1
             else:
-                d[index][row[7]] += 1
+                d[key][row[7]] += 1
 
-#Code block for aggregation functions
+# Code block for aggregate functions
 
-for k in d:
-    l = [0, 0, 0]
-    for c in d[k]:
-        l[0] += d[k][c] #num complaints
-        l[1] += 1 #num companies
-        if l[2] < d[k][c]: # highest count
-            l[2] = d[k][c]
-    l[2] = round(float(l[2])/float(l[0]) * 100) #CAREFUL, THIS ROUNDS DOWN FOR .5
-    d[k] = l
+for key in d:
+    stats = [0, 0, 0]
+    for c in d[key]:
+        stats[0] += d[key][c] # Recurrence of this complaint
+        stats[1] += 1 # Number of companies with this complaint
+        if stats[2] < d[key][c]: # The number of complaints for the company with the most complaints
+            stats[2] = d[key][c]
+    stats[2] = round(float(stats[2])/float(stats[0]) * 100) # CAREFUL! THIS ROUNDS DOWN FOR .5!
+    d[key] = stats
 
-#Code block to order the data & prepare for writing to output file
+# Code block to sort, then write to output file
+
 l = d.items()
-l = sorted(l, key = lambda x: int(x[0][1])) #Sort by year
-l = sorted(l, key = lambda x: x[0][0]) #Sort by product
+l = sorted(l, key = lambda x: int(x[0][1])) # Sort by year
+l = sorted(l, key = lambda x: x[0][0]) # Sort by product
 
-
-o = sys.argv[2] #output
-with open(o, mode='w') as file:
+with open(sys.argv[2], mode='w') as file: # Open output file
     writer = csv.writer(file)
     for r in l:
-        writer.writerow([r[0][0],
-                         r[0][1],
-                         str(r[1][0]),
-                         str(r[1][1]),
-                         str(r[1][2])])
+        writer.writerow([r[0][0], # Product
+                         r[0][1], # Year
+                         str(r[1][0]), # Number of complaints
+                         str(r[1][1]), # Number of companies with complaint
+                         str(r[1][2])]) # Highest percentage of complaints for one company
